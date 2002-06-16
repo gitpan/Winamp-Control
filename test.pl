91ed6db6c6d6d9a914fde386bce3c2b036496719
Murat Ünalan <muenalan@cpan.org>
2002-06-16 10:02:41 +0000

    Import of MUENALAN/Winamp-Control-0.01 from CPAN.
    gitpan-cpan-distribution: Winamp-Control
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MUENALAN/Winamp-Control-0.01.tar.gz
    gitpan-cpan-author:       MUENALAN
    gitpan-cpan-maturity:     released

---
+++ test.pl
@@ -0,0 +1,71 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
+
+#########################
+
+# change 'tests => 1' to 'tests => last_test_to_print';
+
+use Test;
+
+BEGIN { plan tests => 2 };
+
+use Winamp::Control;
+
+ok(1); # If we made it this far, we're ok.
+
+print <<ENDE;
+
+You will need the winamp-plugin "httpQ" (Written by Kosta Arvanitis) installed on the machine playing
+the music via winamp/shoutcast. You may find it via searching at the winamp headquarter:
+
+	http://www.winamp.com
+
+Or directly from the author (for the recent version):
+
+	http://www.csc.uvic.ca/~karvanit/
+	http://www.csc.uvic.ca/~karvanit/downloads/httpq19.exe
+
+After installation you have to activate httpQ through the winamp preferences dialog. In the
+"Plug-ins/General Purpose" section, select httpQ and press the configure button at the bottom.
+
+Install and run it before the this test, otherwise it will fail (not important).
+
+ENDE
+
+		print 'Enter the host:port where winamp is installed and httpQ is running [localhost:4800]: ';
+
+		chomp( my $input = <STDIN> );
+
+		my ( $host, $port ) = split ':', $input;
+
+		my $winamp = Winamp::Control->new( host => $host || '127.0.0.1', port => $port || 4800 );
+
+		if( my $ver = $winamp->getversion )
+		{
+			printf "\nConnected to Winamp (Ver: %s)\n", $ver;
+
+			print "Current playlist:\n", join "\n\t", $winamp->getplaylisttitle();
+
+			printf "\n\nCurrently playing: %s (%s)\n", $winamp->getcurrenttitle(), $winamp->getplaylistfile( a => $winamp->getlistpos ) if $winamp->isplaying();
+		}
+		else
+		{
+
+print <<ENDE;
+
+TEST FAILED:
+
+	1) If winamp was started.
+	2) If the httpQ-plugin was started (explicitly started httpQ with the 'start'-button).
+	3) If you have entered the correct host and port.
+
+ENDE
+		}
+
+ok(2);
+
+#########################
+
+# Insert your test code below, the Test module is use()ed here so read
+# its man page ( perldoc Test ) for help writing this test script.
+
---
+++ winampctl.pl
@@ -0,0 +1,98 @@
+#!/usr/bin/perl
+#
+# Author: Murat Uenalan (muenalan@cpan.org)
+#
+# Copyright (c) 2002 Murat Uenalan. All rights reserved.
+# Note: This program is free software; you can redistribute
+# it and/or modify it under the same terms as Perl itself.
+
+our $VERSION = '0.01';
+
+use warnings; use strict;
+
+## Parse options and print usage if there is a syntax error,
+## or if usage was explicitly requested.
+
+use Getopt::Long;
+
+use Pod::Usage;
+
+our %opts = ( host => 'localhost', port => 4800 );
+
+	GetOptions (\%opts, 'help|?', 'man', 'debug!', 'port=i', 'host=s' ) or pod2usage(2);
+
+	pod2usage(1) if $opts{help};
+
+	#pod2usage( -verbose => 2 ) if $opts{man};
+
+	qx{perldoc Winamp::Control} if $opts{man};
+
+##
+##
+
+use Winamp::Control;
+
+	my $winamp = Winamp::Control->new( host => $opts{host}, port => $opts{port} );
+
+	if( my $ver = $winamp->getversion )
+	{
+		printf "\nConnected to Winamp (Ver: %s)\n", $ver;
+
+		print "Current playlist:\n\t", join "\n\t", $winamp->getplaylisttitle(), "\n";
+
+		printf "\nCurrently playing: %s\n", $winamp->getcurrenttitle() if $winamp->isplaying();
+
+		print "File entry:\n\t", $winamp->getplaylistfile( a => $winamp->getlistpos ), "\n";
+
+		my $com = shift @ARGV;
+
+		println "Returned ($com): " , $winamp->$com( ) if $com;
+	}
+	else
+	{
+		warn 'Unable to connect to winamp/httpQ';
+	}
+
+__END__
+
+=head1 NAME
+
+winampctl - control winamp over a network
+
+=head1 SYNOPSIS
+
+winamp [options] [command ...]
+
+ Options:
+   -help            brief help message
+   -man             full documentation
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-help>
+
+Print a brief help message and exits.
+
+=item B<-man>
+
+Prints the manual page and exits.
+
+=item B<-host>
+
+Host running winamp/httpQ (default: localhost).
+
+=item B<-port>
+
+Port to connect to httpQ (default: 4800).
+
+=back
+
+=head1 DESCRIPTION
+
+B<winampctl> will simply call the command as a method to the Winamp::Control
+object, without any sanity checks.
+
+=cut
+
